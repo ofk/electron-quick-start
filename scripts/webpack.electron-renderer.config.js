929ae64f2039af3abfe96f5883adfe3fc8a00091
ofk <ofkjpn+github@gmail.com>
2018-06-14 13:54:41 +0900

    Add CSS build system

--- scripts/webpack.electron-renderer.config.js
+++ scripts/webpack.electron-renderer.config.js
@@ -1,11 +1,35 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const config = require('./webpack.base.config');
 
 module.exports = {
-  ...require('./webpack.base.config'),
+  ...config,
   entry: {
     renderer: [
       path.resolve(__dirname, '..', 'src', 'renderer.js'),
+    ],
+  },
+  module: {
+    rules: [
+      ...config.module.rules,
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|eot|otf|ttf|woff2?)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+          },
+        },
+      },
     ],
   },
   plugins: [
@@ -24,6 +48,10 @@ module.exports = {
         sortClassName: true,
         useShortDoctype: true,
       },
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
     }),
   ],
   target: 'electron-renderer',
